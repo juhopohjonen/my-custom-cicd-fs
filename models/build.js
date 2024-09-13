73b432811286ab693da502a26b29b736c5302308
Juho <juhopohjonen237@gmail.com>
2024-09-13 22:27:34 +0300

    add /builds api for test checking

---
+++ models/build.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const buildSchema = mongoose.Schema({
+    commitId: String,
+    githubUsername: String
+})
+
+buildSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+const Build = mongoose.model('Build', buildSchema)
+
+module.exports = Build
